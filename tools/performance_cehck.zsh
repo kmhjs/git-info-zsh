e53691d24d758cc04a7b19d2670609007732482c
K. Murakami <kmhjs@users.noreply.github.com>
2016-12-30 14:09:07 +0900

    MOD: Syntax and indent

--- tools/performance_cehck.zsh
+++ tools/performance_cehck.zsh
@@ -1,37 +1,40 @@
 #! /usr/bin/env zsh
 
 function milliseconds_unix_time() {
-    echo $(($(date +%s%N) / 1000000.0))
+  echo $(($(date +%s%N) / 1000000.0))
 }
 
 function show_proc_time() {
-    cmd=${1}
+  local cmd
+  cmd=${1}
 
-    local avg_time
-    avg_time=0
+  local avg_time
+  avg_time=0
 
-    local number_of_samples=5
+  local number_of_samples=5
 
-    local start_time elapsed_time
-    for i ({1..${number_of_samples}})
-    do
-      start_time=$(milliseconds_unix_time)
-      ${cmd} 1>/dev/null 2>/dev/null
-      elapsed_time=$(($(milliseconds_unix_time) - ${start_time}))
+  local start_time elapsed_time
+  for i ({1..${number_of_samples}})
+  do
+    start_time=$(milliseconds_unix_time)
+    ${cmd} 1>/dev/null 2>/dev/null
+    elapsed_time=$(($(milliseconds_unix_time) - ${start_time}))
 
-      avg_time=$((${avg_time} + ${elapsed_time}))
-    done
+    avg_time=$((${avg_time} + ${elapsed_time}))
+  done
 
-    avg_time=$((${avg_time} / ${number_of_samples}))
+  avg_time=$((${avg_time} / ${number_of_samples}))
 
-    printf "%40s : time (%d times avg) -> %5f ms\n" ${cmd:t} ${number_of_samples} ${avg_time}
+  printf "%40s : time (%d times avg) -> %5f ms\n" ${cmd:t} ${number_of_samples} ${avg_time}
 }
 
 # Load
-for f ($(find . -iname 'git-*')); do
-    source ${f}
-; done
+for f ($(find . -iname 'git-*'))
+do
+  source ${f}
+done
 
-for f ($(find . -iname 'git-*')); do
-    show_proc_time $(echo ${f} | tr -d './')
-; done
+for f ($(find . -iname 'git-*'))
+do
+  show_proc_time $(echo ${f} | tr -d './')
+done
